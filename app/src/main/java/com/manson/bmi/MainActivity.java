dae39a5f191046c297be4ae4c64522c0671ae4ec
mansonyu <mansonyu0204@gmail.com>
2019-01-29 12:44:52 +0800

    加入log的練習

--- app/src/main/java/com/manson/bmi/MainActivity.java
+++ app/src/main/java/com/manson/bmi/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         float weight_float = Float.parseFloat(w);
         float height_float = Float.parseFloat(h);
         float bmi = weight_float / (height_float * height_float);
+        Log.d("MainActivity", "bmi: " + bmi);
         Intent intent = new Intent(this, ResultActivity.class);
         intent.putExtra("BMI", bmi);
         startActivity(intent);
